517300bb2f263a554922edfd0770338cac3641eb
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-03-25 19:41:25 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -2,7 +2,10 @@
 def levitation_quiz
 	#your code here
 	puts "What is the spell that enacts levitation?"
-	
+	answer = gets.chomp
+	break if == "Wingardium Leviosa"
 end
+
+puts "You passed the quiz!"
 
 
